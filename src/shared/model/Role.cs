7e085aba5fe5bae17dcaf8606e37ebf822a56dce
longletranduc <longdepzaivcl151@gmail.com>
2026-09-22 19:47:52 +0700

    Cập nhật code logic mới

---
+++ src/shared/model/Role.cs
@@ -0,0 +1,10 @@
+namespace SharedLibrary
+{
+    public enum Role
+    {
+        WEREWOLF,  // Ma Sói
+        SEER,      // Tiên Tri
+        BODYGUARD, // Bảo Vệ
+        VILLAGER   // Dân Làng
+    }
+}
